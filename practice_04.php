99eee4300a0ac6ca5104fff393acc298753fd4b5
keita <ozakeee1127@ezweb.ne.jp>
2020-07-10 15:01:25 +0000

    add

--- practice_04.php
+++ practice_04.php
@@ -28,6 +28,55 @@ foreach($arr as $a){
   echo "\n";
   
 //課題4
+function max_array($arr){
+  $max_number = $arr[0];
+  foreach($arr as $a){
+   if($max_number < $a){
+     $max_number = $a;
+   }
+  }
+  return $max_number;
+  }
+  echo max_array(array(11, 27, 4, 18, 6, 1));
+  echo "\n";
 
+//課題5
+//strip_tags
+$str = "<p>タグを取り除きました。</p>";
+  echo strip_tags($str);
+  echo "\n";
+  
+//array_push
+$numbers = ["一番","二番","三番"];
+  array_push($numbers,"四番","五番");
+  print_r($numbers);
+  
+//array_merge
+$array1 = [1, 2, 3];
+$array2 = [4, 5, 6];
+$array3 = [7, 8, 9];
+  $array = array_merge($array1, $array2, $array3);
+  print_r($array);
+
+//time
+$timestamp = time();
+  echo $timestamp;
+  echo "\n";
+  
+//mktime
+date_default_timezone_set("UTC");
+$timestamp = mktime(0, 0, 0, 27, 11, 2019);
+  echo $timestamp;
+  echo "\n";
+  
+//date
+echo date('Y/m/d');
+echo "\n";
+ 
+// 現在日時をフォーマットする
+echo date("Y-m-d H:i:s");
+echo "\n";
+echo date("Y年m月d日 H時i分s秒");
+echo "\n";
 
 ?>
